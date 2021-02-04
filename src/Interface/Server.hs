bf3de6f369cde37ae0c1c7fc4f33854bf5c3fcf5
peigongdsd <peigong2013@outlook.com>
2021-02-04 15:03:05 +0800

    server

--- src/Interface/Server.hs
+++ src/Interface/Server.hs
@@ -1,3 +1,32 @@
-module Interface.Server () where
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
 
+module Interface.Server ( app ) where
 
+import Yesod
+import Yesod.EmbeddedStatic
+    ( embedStaticContent, mkEmbeddedStatic, embedDir, EmbeddedStatic )
+
+mkEmbeddedStatic False "nodeModules" [embedDir "./src/Interface/node_modules/xterm"]
+
+data ServerApp = ServerApp { getStatic :: EmbeddedStatic }
+
+mkYesod "ServerApp" [parseRoutes|
+/ HomeR GET
+/static StaticR EmbeddedStatic getStatic
+|]
+
+instance Yesod ServerApp where
+    addStaticContent = embedStaticContent getStatic StaticR Right
+
+getHomeR :: Handler Html 
+getHomeR = defaultLayout $ 
+    toWidgetHead [hamlet|
+    <p>Hello!!
+    <a href=@css_xterm_css> what?
+    |]
+
+app :: ServerApp
+app = ServerApp nodeModules
